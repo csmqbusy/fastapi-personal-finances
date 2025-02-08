69dd3e7ac0ef24d14fbec88c74024cfac7f635de
csmqbusy <cosmiqeyes@yandex.by>
2025-02-08 15:30:31 +0300

    refactor: add tags to `authentication_router`

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,5 +1,10 @@
 from fastapi import APIRouter
-from .routes import authentication_router
+from .routes import (
+    authentication_router,
+)
 
 router_v1 = APIRouter()
-router_v1.include_router(authentication_router)
+router_v1.include_router(
+    authentication_router,
+    tags=["authentication"],
+)
