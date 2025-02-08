701d9a62b640c181cf2d1ca2e2d3dc55792bc112
csmqbusy <cosmiqeyes@yandex.by>
2025-02-08 16:56:55 +0300

    feat: add `SpendingsModel`

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,10 +1,12 @@
 from .base_model import Base
 from .user_model import UserModel
 from .spending_categories_model import SpendingCategoriesModel
+from .spendings_model import SpendingsModel
 
 
 __all__ = [
     "Base",
     "UserModel",
     "SpendingCategoriesModel",
+    "SpendingsModel",
 ]
---
+++ app/models/spendings_model.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from sqlalchemy import ForeignKey, String, text
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.models import Base
+from app.models.mixins import IdIntPKMixin
+
+
+class SpendingsModel(IdIntPKMixin, Base):
+    __tablename__ = "spendings"
+
+    amount: Mapped[float] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(String(100), nullable=True)
+    date: Mapped[datetime] = mapped_column(
+        server_default=text("TIMEZONE ('utc', now())"),
+        nullable=False,
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+        nullable=False,
+    )
+    category_id: Mapped[int] = mapped_column(
+        ForeignKey("spending_categories.id"),
+        nullable=False,
+    )
