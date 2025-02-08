d37cc9736deb31ec603e6221ce3b3fa784c59fbe
csmqbusy <cosmiqeyes@yandex.by>
2025-02-08 15:31:41 +0300

    feat: add mock version of `operations_routes`

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter
 from .routes import (
     authentication_router,
+    operations_router,
 )
 
 router_v1 = APIRouter()
 router_v1.include_router(
     authentication_router,
     tags=["authentication"],
+)
+router_v1.include_router(
+    operations_router,
+    tags=["operations"],
 )
--- app/api/v1/routes/__init__.py
+++ app/api/v1/routes/__init__.py
@@ -1,6 +1,8 @@
 from .auth_routes import router as authentication_router
+from .operations_routes import router as operations_router
 
 
 __all__ = [
-    "authentication_router"
+    "authentication_router",
+    "operations_router",
 ]
