9f44190e54339cc2624b653892c389769a35381b
csmqbusy <cosmiqeyes@yandex.by>
2025-02-09 17:49:35 +0300

    feat: add objects attributes support to `SSpendingBase` schema

--- app/schemas/spendings_schemas.py
+++ app/schemas/spendings_schemas.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 
 
 class SSpendingBase(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     amount: float
     description: str | None = Field(None, max_length=100)
     category_name: str | None = Field(None, max_length=50)
