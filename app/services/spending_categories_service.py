c0cf906ca94a65864de858e79f46809f67e37258
csmqbusy <cosmiqeyes@yandex.by>
2025-02-09 19:04:32 +0300

    feat: add `out_schema` param to `SpendingCategoriesService` init

--- app/services/spending_categories_service.py
+++ app/services/spending_categories_service.py
@@ -1,14 +1,20 @@
 from app.core.config import settings
 from app.repositories import spend_cat_repo
+from app.schemas.spending_category_schemas import SSpendingCategoryOut
 from app.services.base_categories_service import BaseCategoriesService
 
 
 class SpendingCategoriesService(BaseCategoriesService):
-    def __init__(self, category_repo, default_category_name):
-        super().__init__(category_repo, default_category_name)
+    def __init__(self, category_repo, default_category_name, out_schema):
+        super().__init__(
+            category_repo,
+            default_category_name,
+            out_schema,
+        )
 
 
 spend_cat_service = SpendingCategoriesService(
     spend_cat_repo,
     settings.app.default_spending_category_name,
+    SSpendingCategoryOut,
 )
