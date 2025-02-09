7c1684b19544e388313584487cc71163d41cbf3e
csmqbusy <cosmiqeyes@yandex.by>
2025-02-09 18:01:37 +0300

    refactor: remove `category_name` param from `SSpendingBase` schema

--- app/schemas/spendings_schemas.py
+++ app/schemas/spendings_schemas.py
@@ -8,7 +8,6 @@ class SSpendingBase(BaseModel):
 
     amount: float
     description: str | None = Field(None, max_length=100)
-    category_name: str | None = Field(None, max_length=50)
 
 
 class SSpendingIn(SSpendingBase):
