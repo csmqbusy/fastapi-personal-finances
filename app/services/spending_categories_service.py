d86033e54a96796435abc1233f0074280aa37f2b
csmqbusy <cosmiqeyes@yandex.by>
2025-02-10 20:56:06 +0300

    feat: add `user_categories_repo` argument for `SpendingCategoriesService` initializing

--- app/services/spending_categories_service.py
+++ app/services/spending_categories_service.py
@@ -1,20 +1,28 @@
 from app.core.config import settings
-from app.repositories import spend_cat_repo
+from app.repositories import spend_cat_repo, user_spend_cat_repo
 from app.schemas.spending_category_schemas import SSpendingCategoryOut
 from app.services.base_categories_service import BaseCategoriesService
 
 
 class SpendingCategoriesService(BaseCategoriesService):
-    def __init__(self, category_repo, default_category_name, out_schema):
+    def __init__(
+        self,
+        category_repo,
+        user_categories_repo,
+        default_category_name,
+        out_schema,
+    ):
         super().__init__(
-            category_repo,
-            default_category_name,
-            out_schema,
+            category_repo=category_repo,
+            user_categories_repo=user_categories_repo,
+            default_category_name=default_category_name,
+            out_schema=out_schema,
         )
 
 
 spend_cat_service = SpendingCategoriesService(
     spend_cat_repo,
+    user_spend_cat_repo,
     settings.app.default_spending_category_name,
     SSpendingCategoryOut,
 )
