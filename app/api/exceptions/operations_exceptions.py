d860920f05c62dadcb89c13e39bdf1cd000f21b9
csmqbusy <cosmiqeyes@yandex.by>
2025-02-13 11:55:58 +0300

    feat: add `CategoryAlreadyExistsError` HTTPException

--- app/api/exceptions/operations_exceptions.py
+++ app/api/exceptions/operations_exceptions.py
@@ -7,3 +7,11 @@ class SpendingNotFoundError(HTTPException):
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Spending not found.",
         )
+
+
+class CategoryAlreadyExistsError(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="You already have a category with that name.",
+        )
