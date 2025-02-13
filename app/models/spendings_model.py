87681c5049880d955df806aae80d53d9ab661ca4
csmqbusy <cosmiqeyes@yandex.by>
2025-02-13 12:48:03 +0300

    feat: remove `spendings` with `spending_categories` relationship

--- app/models/spendings_model.py
+++ app/models/spendings_model.py
@@ -1,15 +1,10 @@
 from datetime import datetime
-from typing import TYPE_CHECKING
 
 from sqlalchemy import ForeignKey, String, text
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.models import Base
 from app.models.mixins import IdIntPKMixin
-
-
-if TYPE_CHECKING:
-    from app.models import SpendingCategoriesModel
 
 
 class SpendingsModel(IdIntPKMixin, Base):
@@ -24,9 +19,4 @@ class SpendingsModel(IdIntPKMixin, Base):
     user_id: Mapped[int] = mapped_column(
         ForeignKey("users.id", ondelete="CASCADE"),
         nullable=False,
-    )
-
-    spending_category: Mapped["SpendingCategoriesModel"] = relationship(
-        "SpendingCategoriesModel",
-        lazy="joined"
     )
