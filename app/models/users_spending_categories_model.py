f22dfec04181b284d00cae3dfbec634fd8e37c89
csmqbusy <cosmiqeyes@yandex.by>
2025-02-13 12:48:43 +0300

    feat: add `category_name` field max length

--- app/models/users_spending_categories_model.py
+++ app/models/users_spending_categories_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, UniqueConstraint
+from sqlalchemy import ForeignKey, UniqueConstraint, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.models import Base
@@ -12,7 +12,7 @@ class UsersSpendingCategoriesModel(IdIntPKMixin, Base):
         ForeignKey("users.id", ondelete="CASCADE"),
     )
 
-    category_name: Mapped[str] = mapped_column()
+    category_name: Mapped[str] = mapped_column(String(50))
 
     __table_args__ = (
         UniqueConstraint("user_id", "category_name", name="uq_user_category"),
