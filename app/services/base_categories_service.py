bd7df742a5c86fcba4d0960c24d5079503dba31d
csmqbusy <cosmiqeyes@yandex.by>
2025-02-14 15:36:04 +0300

    feat: add `get_category` method in `BaseCategoriesService`

--- app/services/base_categories_service.py
+++ app/services/base_categories_service.py
@@ -1,7 +1,7 @@
 from typing import Generic, TypeVar, Type
 
 from pydantic import BaseModel
-
+from sqlalchemy.ext.asyncio import AsyncSession
 
 T = TypeVar('T')
 
@@ -16,3 +16,15 @@ class BaseCategoriesService(Generic[T]):
         self.category_repo = category_repo
         self.default_category_name = default_category_name
         self.out_schema = out_schema
+
+    async def get_category(
+        self,
+        user_id: int,
+        category_name: str,
+        session: AsyncSession,
+    ) -> T | None:
+        category = await self.category_repo.get_one_by_filter(
+            session,
+            dict(user_id=user_id, category_name=category_name),
+        )
+        return category
