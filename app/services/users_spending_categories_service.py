8d64d4e393c8bddf55ac158e131a74ef7853ea19
csmqbusy <cosmiqeyes@yandex.by>
2025-02-14 15:41:52 +0300

    feat: init `UserSpendingCategoriesService` from `BaseCategoriesService`

--- app/services/users_spending_categories_service.py
+++ app/services/users_spending_categories_service.py
@@ -1,14 +1,25 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-
+from app.core.config import settings
 from app.repositories import user_spend_cat_repo
+from app.schemas.spending_category_schemas import SSpendingCategoryOut
+from app.services.base_categories_service import BaseCategoriesService
 
 
-async def get_user_categories(
-    user_id: int,
-    session: AsyncSession,
-):
-    user_categories = await user_spend_cat_repo.get_all_by_filter(
-        session,
-        dict(user_id=user_id),
-    )
-    return user_categories
+class UserSpendingCategoriesService(BaseCategoriesService):
+    def __init__(
+        self,
+        category_repo,
+        default_category_name: str,
+        out_schema,
+    ):
+        super().__init__(
+            category_repo=category_repo,
+            default_category_name=default_category_name,
+            out_schema=out_schema,
+        )
+
+
+user_spend_cat_service = UserSpendingCategoriesService(
+    user_spend_cat_repo,
+    settings.app.default_spending_category_name,
+    SSpendingCategoryOut,
+)
