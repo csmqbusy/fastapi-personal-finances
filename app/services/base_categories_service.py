69302c429adecdc44e4ea5b952ca60b2d4da4271
csmqbusy <cosmiqeyes@yandex.by>
2025-02-15 16:29:19 +0300

    feat: add new `delete_category` method to `BaseCategoriesService`

--- app/services/base_categories_service.py
+++ app/services/base_categories_service.py
@@ -6,8 +6,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.exceptions.categories_exceptions import (
     CategoryAlreadyExists,
     CategoryNotFound,
+    CategoryNameNotFound,
+    CannotDeleteDefaultCategory,
 )
-from app.schemas.spending_category_schemas import SSpendingCategoryUpdate
+from app.schemas.spending_category_schemas import (
+    SSpendingCategoryUpdate,
+    SpendingsOnDeleteActions,
+)
+
 
 T = TypeVar('T')
 
@@ -121,6 +127,59 @@ class BaseCategoriesService(Generic[T]):
             params=dict(category_name=category_update_obj.category_name),
         )
         return self.out_schema.model_validate(updated_category)
+
+    async def delete_category(
+        self,
+        category_name: str,
+        user_id: int,
+        transactions_actions: SpendingsOnDeleteActions,
+        new_category_name: str | None,
+        session: AsyncSession,
+    ):
+        if category_name == self.default_category_name:
+            raise CannotDeleteDefaultCategory
+
+        category_for_delete = await self.get_category(
+            user_id, category_name, session)
+        if category_for_delete is None:
+            raise CategoryNotFound
+
+        transactions = await self.transaction_repo.get_all_by_filter(
+            session, dict(category_id=category_for_delete.id, user_id=user_id))
+
+        if transactions_actions == SpendingsOnDeleteActions.DELETE:
+            for spending in transactions:
+                await self.transaction_repo.delete(session, spending.id)
+        elif transactions_actions == SpendingsOnDeleteActions.TO_DEFAULT:
+            default_category = await self.get_default_category(user_id, session)
+            await self.change_transactions_category(
+                transactions,
+                default_category.id,
+                session,
+            )
+        else:
+            if new_category_name is None:
+                raise CategoryNameNotFound
+            if transactions_actions == SpendingsOnDeleteActions.TO_NEW_CAT:
+                await self.add_category_to_db(
+                    user_id,
+                    new_category_name,
+                    session,
+                )
+            category = await self.get_category(
+                user_id,
+                new_category_name,
+                session,
+            )
+            if category is None:
+                raise CategoryNotFound
+
+            await self.change_transactions_category(
+                transactions,
+                category.id,
+                session,
+            )
+        await self.category_repo.delete(session, category_for_delete.id)
 
     async def change_transactions_category(
         self,
