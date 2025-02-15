0d97b42cc81940b54467ab8206b9a36496d4755c
csmqbusy <cosmiqeyes@yandex.by>
2025-02-15 16:16:11 +0300

    fix: change `CategoryNotFoundError` detail text

--- app/api/exceptions/operations_exceptions.py
+++ app/api/exceptions/operations_exceptions.py
@@ -21,7 +21,7 @@ class CategoryNotFoundError(HTTPException):
     def __init__(self):
         super().__init__(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="Create this category before adding operations to it.",
+            detail="Create this category before performing operations on it.",
         )
 
 
