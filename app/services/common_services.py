0a7938ea5442290e75b164b8e4eec3d1fbdb27d7
csmqbusy <cosmiqeyes@yandex.by>
2025-02-16 17:15:38 +0300

    feat: add new `common_services.py` module with `apply_pagination` function

---
+++ app/services/common_services.py
@@ -0,0 +1,12 @@
+from typing import Sequence
+
+from app.schemas.pagination_schemas import SPagination
+
+
+def apply_pagination(
+    data: Sequence,
+    pagination: SPagination,
+):
+    start = (pagination.page - 1) * pagination.page_size
+    stop = pagination.page * pagination.page_size
+    return data[start:stop]
