891b6afd13d878740967a91a14651e2e7b526f18
csmqbusy <cosmiqeyes@yandex.by>
2025-02-16 14:01:43 +0300

    refactor: remove `UserSpendingCategoriesService`, init `user_spend_cat_service` from `BaseCategoriesService`

--- app/services/users_spending_categories_service.py
+++ app/services/users_spending_categories_service.py
@@ -4,25 +4,9 @@ from app.schemas.spending_category_schemas import SSpendingCategoryOut
 from app.services.base_categories_service import BaseCategoriesService
 
 
-class UserSpendingCategoriesService(BaseCategoriesService):
-    def __init__(
-        self,
-        category_repo,
-        transaction_repo,
-        default_category_name: str,
-        out_schema,
-    ):
-        super().__init__(
-            category_repo=category_repo,
-            transaction_repo=transaction_repo,
-            default_category_name=default_category_name,
-            out_schema=out_schema,
-        )
-
-
-user_spend_cat_service = UserSpendingCategoriesService(
-    user_spend_cat_repo,
-    spendings_repo,
-    settings.app.default_spending_category_name,
-    SSpendingCategoryOut,
+user_spend_cat_service = BaseCategoriesService(
+    category_repo=user_spend_cat_repo,
+    transaction_repo=spendings_repo,
+    default_category_name=settings.app.default_spending_category_name,
+    out_schema=SSpendingCategoryOut,
 )
