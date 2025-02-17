0cbc17e0b45ff48fe5d14504040224f56fb6e4a1
csmqbusy <cosmiqeyes@yandex.by>
2025-02-17 23:11:30 +0300

    feat: add `get_transactions_query_params` dependency

--- app/api/dependencies/operations_dependencies.py
+++ app/api/dependencies/operations_dependencies.py
@@ -4,6 +4,7 @@ from fastapi import Query
 
 from app.schemas.date_range_schemas import SDateRange
 from app.schemas.pagination_schemas import SPagination
+from app.schemas.spendings_schemas import STransactionsQueryParams
 
 
 def get_pagination_params(
@@ -23,4 +24,27 @@ def get_date_range(
     return SDateRange(
         start=date_from,
         end=date_to,
+    )
+
+
+def get_transactions_query_params(
+    category_id: int | None = Query(
+        None,
+        description=(
+            "The priority way to identify a category. "
+            "If there is conflicting information in category_id and "
+            "category_name, category_id will be used."
+        ),
+    ),
+    category_name: str | None = Query(
+        None,
+        description=(
+            "Specify the category name if the id is unknown. "
+            "Ignore the category_id parameter if you use category_name."
+        ),
+    ),
+) -> STransactionsQueryParams:
+    return STransactionsQueryParams(
+        category_id=category_id,
+        category_name=category_name,
     )
