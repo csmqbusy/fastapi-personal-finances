bb814a04ab50b6775c81944f5ea64f37c2b97fe0
csmqbusy <cosmiqeyes@yandex.by>
2025-02-17 18:34:26 +0300

    feat: add `get_date_range` dependency

--- app/api/dependencies/operations_dependencies.py
+++ app/api/dependencies/operations_dependencies.py
@@ -1,5 +1,8 @@
+from datetime import date
+
 from fastapi import Query
 
+from app.schemas.date_range_schemas import SDateRange
 from app.schemas.pagination_schemas import SPagination
 
 
@@ -10,4 +13,14 @@ def get_pagination_params(
     return SPagination(
         page=page,
         page_size=page_size,
+    )
+
+
+def get_date_range(
+    start_date: date = Query(None),
+    end_date: date = Query(None)
+) -> SDateRange:
+    return SDateRange(
+        start=start_date,
+        end=end_date,
     )
