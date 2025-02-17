81f2289c36f90fd8b14b6fe7a797eb4ff49775af
csmqbusy <cosmiqeyes@yandex.by>
2025-02-17 23:15:19 +0300

    feat: add `get_transactions` method
    - the most advanced method for obtaining a list of transactions, which may expand in the future and replace other methods.

--- app/repositories/spendings_repository.py
+++ app/repositories/spendings_repository.py
@@ -1,3 +1,6 @@
+from datetime import date
+from typing import Literal
+
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
@@ -35,6 +38,32 @@ class SpendingsRepository(BaseRepository[SpendingsModel]):
             session=session,
         )
         return result
+
+    async def get_transactions(
+        self,
+        session: AsyncSession,
+        query_params: dict,
+        date_from: date | None = None,
+        date_to: date | None = None,
+        order_by: str | None = None,
+        order_direction: Literal["asc", "desc"] = "asc",
+    ):
+        query = select(self.model).filter_by(
+            **query_params)
+
+        if date_from:
+            query = query.where(self.model.date >= date_from)
+        if date_to:
+            query = query.where(self.model.date <= date_to)
+
+        if order_by:
+            if order_direction == "asc":
+                query = query.order_by(getattr(self.model, order_by).asc())
+            else:
+                query = query.order_by(getattr(self.model, order_by).desc())
+
+        result = await session.execute(query)
+        return list(result.scalars().all())
 
 
 spendings_repo = SpendingsRepository()
