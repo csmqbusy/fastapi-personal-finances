802808e4de0b1f2a2e38d9b052df1ae2cb4c1298
csmqbusy <cosmiqeyes@yandex.by>
2025-02-19 14:24:10 +0300

    feat: add validator for `category_name` in   `SSpendingCategoryBase`, which removes spaces at the edges

--- app/schemas/spending_category_schemas.py
+++ app/schemas/spending_category_schemas.py
@@ -1,12 +1,16 @@
 from enum import Enum
 
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, Field, ConfigDict, field_validator
 
 
 class SSpendingCategoryBase(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     category_name: str = Field(..., max_length=50)
+
+    @field_validator("category_name")
+    def validate_category_name(cls, value):
+        return value.strip()
 
 
 class SSpendingCategoryCreate(SSpendingCategoryBase):
