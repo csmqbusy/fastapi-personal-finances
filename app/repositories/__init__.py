d18dcc98e31d0398a857644a8dfd560d47f7fd01
csmqbusy <cosmiqeyes@yandex.by>
2025-02-19 17:10:09 +0300

    chore: update `__init__.py` in repositories package

--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -1,9 +1,11 @@
 from .user_repository import user_repo
+from .base_transactions_repository import BaseTransactionsRepository
 from .spendings_repository import spendings_repo
 from .users_spending_categories_repository import user_spend_cat_repo
 
 __all__ = [
     "user_repo",
+    "BaseTransactionsRepository",
     "spendings_repo",
     "user_spend_cat_repo",
 ]
