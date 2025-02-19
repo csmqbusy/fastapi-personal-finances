d98b6bdd5b79a9e71eba80e462e60c2ce0c45e0c
csmqbusy <cosmiqeyes@yandex.by>
2025-02-19 13:25:11 +0300

    feat: add `get_by_username` method to `UserRepository`

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select
+
 from app.models import UserModel
 from app.repositories.base_repository import BaseRepository
 
@@ -5,6 +7,14 @@ from app.repositories.base_repository import BaseRepository
 class UserRepository(BaseRepository[UserModel]):
     def __init__(self):
         super().__init__(UserModel)
+
+    async def get_by_username(self, session, username):
+        query = select(self.model).filter(
+            self.model.username.ilike(f"%{username}%")
+        )
+        user = await session.execute(query)
+        return user.scalar_one_or_none()
+
 
 
 user_repo = UserRepository()
