c9f3082eb73735f758c7c882a960c012ba59aff6
csmqbusy <cosmiqeyes@yandex.by>
2025-02-20 16:22:44 +0300

    refactor: delete unused `users_spending_categories_schema.py` module

--- app/schemas/users_spending_categories_schema.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, ConfigDict
-
-
-class SUserSpendingCategories(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
-
-    user_id: int
-    category_id: int
