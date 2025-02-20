b554f0ea4ca295575b96c52cde5ab41e8b31c718
csmqbusy <cosmiqeyes@yandex.by>
2025-02-20 16:57:05 +0300

    test: add integration test for `user_spend_cat_service.get_category()` method

--- tests/integration/spendings_categories/test_users_spending_categories_service.py
+++ tests/integration/spendings_categories/test_users_spending_categories_service.py
@@ -53,3 +53,51 @@ async def test_add_category_to_db(
         assert type(category) is user_spend_cat_service.out_schema
         assert category.category_name == category_schema.category_name
         assert category.user_id == user.id
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "category_name, expectation, create_user",
+    [
+        (
+            "Food",
+            nullcontext(),
+            True,
+        ),
+        (
+            "  food ",
+            pytest.raises(CategoryAlreadyExists),
+            False,
+        ),
+        (
+            "c@t ***",
+            nullcontext(),
+            False,
+        ),
+    ]
+)
+async def test_get_category(
+    db_session: AsyncSession,
+    category_name: str,
+    expectation: ContextManager,
+    create_user: bool,
+):
+    mock_user_username = "DI_MARIA"
+    if create_user:
+        await add_mock_user(db_session, mock_user_username)
+    user = await user_repo.get_by_username(db_session, mock_user_username)
+
+    category_schema = STransactionCategoryCreate(category_name=category_name)
+    with expectation:
+        await user_spend_cat_service.add_category_to_db(
+            user.id,
+            category_schema.category_name,
+            db_session,
+        )
+        category = await user_spend_cat_service.get_category(
+            user.id,
+            category_schema.category_name,
+            db_session,
+        )
+        assert category.category_name == category_schema.category_name
+        assert category.user_id == user.id
