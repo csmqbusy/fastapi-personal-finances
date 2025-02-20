1d3e676166704cf13f16a98e493e8b682c8a8763
csmqbusy <cosmiqeyes@yandex.by>
2025-02-20 15:45:34 +0300

    fix: fix search terms for `ilike` method

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -10,14 +10,14 @@ class UserRepository(BaseRepository[UserModel]):
 
     async def get_by_username(self, session, username):
         query = select(self.model).filter(
-            self.model.username.ilike(f"%{username}%")
+            self.model.username.ilike(username)
         )
         user = await session.execute(query)
         return user.scalar_one_or_none()
 
     async def get_by_email(self, session, email):
         query = select(self.model).filter(
-            self.model.email.ilike(f"%{email}%")
+            self.model.email.ilike(email)
         )
         user = await session.execute(query)
         return user.scalar_one_or_none()
