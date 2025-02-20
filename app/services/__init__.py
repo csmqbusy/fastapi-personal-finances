01534914f88d3ab139a36d4d974b370556e77c5d
csmqbusy <cosmiqeyes@yandex.by>
2025-02-20 16:02:11 +0300

    chore: add `user_spend_cat_service` to `__init__.py` in `services` package

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,6 +1,8 @@
 from .spendings_service import spendings_service
+from .users_spending_categories_service import user_spend_cat_service
 
 
 __all__ = [
     "spendings_service",
+    "user_spend_cat_service",
 ]
