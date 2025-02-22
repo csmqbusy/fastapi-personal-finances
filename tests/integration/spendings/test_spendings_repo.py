b4255796a9ac22844380d4a68d29af199b39aed5
csmqbusy <cosmiqeyes@yandex.by>
2025-02-22 14:45:16 +0300

    test: add first integration test for `BaseTransactionsRepository.get_transactions()` method
    - test the selection with sorting

--- tests/integration/spendings/test_spendings_repo.py
+++ tests/integration/spendings/test_spendings_repo.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import UsersSpendingCategoriesModel
 from app.repositories import spendings_repo, user_repo
-from app.schemas.transactions_schemas import STransactionCreateInDB
+from app.schemas.transactions_schemas import STransactionCreateInDB, SortParam
 from app.services import user_spend_cat_service
 from tests.integration.helpers import add_mock_user
 
@@ -57,3 +57,61 @@ async def test_get_transaction_with_category(
     assert type(spending_from_db.category) is UsersSpendingCategoriesModel
     assert spending_from_db.category.id == category.id
     assert spending_from_db.category.category_name == category.category_name
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "category_name, create_user, prices, sorted_prices",
+    [
+        (
+            "Food",
+            True,
+            [200, 500, 400, 100, 300],
+            [100, 200, 300, 400, 500],
+        ),
+    ]
+)
+async def test_get_transactions__with_sort(
+    db_session: AsyncSession,
+    category_name: str,
+    create_user: bool,
+    prices: list[int],
+    sorted_prices: list[int],
+) -> None:
+    mock_user_username = "THURAM"
+    if create_user:
+        await add_mock_user(db_session, mock_user_username)
+    user = await user_repo.get_by_username(db_session, mock_user_username)
+
+    category = await user_spend_cat_service.add_category_to_db(
+        user.id,
+        category_name,
+        db_session,
+    )
+
+    for p in prices:
+        transaction_to_create = STransactionCreateInDB(
+            amount=p,
+            description="Some description",
+            date=None,
+            user_id=user.id,
+            category_id=category.id,
+        )
+        await spendings_repo.add(
+            db_session,
+            transaction_to_create.model_dump(),
+        )
+
+    spendings = await spendings_repo.get_transactions(
+        session=db_session,
+        query_params=dict(user_id=user.id, category_id=category.id),
+    )
+    assert [s.amount for s in spendings] == prices
+
+    spendings = await spendings_repo.get_transactions(
+        session=db_session,
+        query_params=dict(user_id=user.id, category_id=category.id),
+        sort_params=[SortParam(order_by="amount", order_direction="asc")]
+    )
+    assert len(spendings) == len(prices)
+    assert [s.amount for s in spendings] == sorted_prices
