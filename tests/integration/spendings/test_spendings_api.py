abbe69faad582a1f37a73918a50de86d2e4f6762
csmqbusy <cosmiqeyes@yandex.by>
2025-02-23 19:34:10 +0300

    test: add test for `GET /spendings/categories/` route

--- tests/integration/spendings/test_spendings_api.py
+++ tests/integration/spendings/test_spendings_api.py
@@ -14,6 +14,7 @@ from app.services.user_service import get_user_by_username
 from tests.integration.helpers import sign_up_user, sign_in_user
 
 
+@pytest.mark.asyncio
 @pytest.mark.parametrize(
     (
         "username",
@@ -99,3 +100,58 @@ async def test_spendings__post(
         response_schema = STransactionResponse.model_validate(response.json())
         assert type(response_schema) is STransactionResponse
 
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    (
+        "username",
+        "categories",
+        "status_code",
+    ),
+    [
+        (
+            "Messi10",
+            ["Balls", "Games", "Food", "Doping"],
+            status.HTTP_200_OK,
+        ),
+        (
+            "Messi20",
+            [],
+            status.HTTP_200_OK,
+        ),
+        (
+            "Messi30",
+            [f"Category_{i}" for i in range(200)],
+            status.HTTP_200_OK,
+        ),
+    ]
+)
+async def test_spendings_categories__get(
+    client: TestClient,
+    db_session: AsyncSession,
+    username: str,
+    categories: list[str],
+    status_code: int,
+):
+    sign_up_user(client, username)
+    sign_in_user(client, username)
+    user = await get_user_by_username(username, db_session)
+
+    for category_name in categories:
+        await user_spend_cat_service.add_category_to_db(
+            user.id,
+            category_name,
+            db_session,
+        )
+
+    categories_response = client.get(
+        url=f"{settings.api.prefix_v1}/spendings/categories/",
+    )
+    assert categories_response.status_code == status_code
+    # +1 is an automatically created category for the rest spendings
+    assert len(categories_response.json()) == len(categories) + 1
+
+    assert type(categories_response.json()) is list
+    categories.append(settings.app.default_spending_category_name)
+    for category in categories_response.json():
+        assert category["category_name"] in categories
