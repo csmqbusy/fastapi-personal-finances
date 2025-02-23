f08d645fbeffd0b1cda67f678b8622bddb74f922
csmqbusy <cosmiqeyes@yandex.by>
2025-02-23 17:03:10 +0300

    test: add test for `STransactionsSortParams` schema

---
+++ tests/integration/spendings/test_transactions_schemas.py
@@ -0,0 +1,38 @@
+import pytest
+
+from app.schemas.transactions_schemas import (
+    STransactionsSortParams,
+)
+
+
+@pytest.mark.parametrize(
+    "sort_params, expected_result",
+    [
+        (
+            ["id", "date", "-category_name"],
+            ["id", "date", "-category_name"],
+        ),
+        (
+            ["-id", "-date", "-category_name"],
+            ["-id", "-date", "-category_name"],
+        ),
+        (
+            ["--id", "--date", "-category_name"],
+            ["-id", "-date", "-category_name"],
+        ),
+        (
+            ["-incorrect", "--date", "-category_name_"],
+            ["-date"],
+        ),
+        (
+            ["-incorrect", "--d-ate", "-category_name_"],
+            [],
+        ),
+    ]
+)
+def test_STransactionsSortParams_schema(
+    sort_params: list[str],
+    expected_result: list[str],
+):
+    schema = STransactionsSortParams(sort_by=sort_params)
+    assert schema.sort_by == expected_result
