e7de26ae2ad115a328d423578ce439eb1896c191
csmqbusy <cosmiqeyes@yandex.by>
2025-02-23 17:45:05 +0300

    test: add new helpers functions `sign_up_user` & `sign_in_user`

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -1,11 +1,34 @@
 from random import randint
 
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.testclient import TestClient
 
+from app.core.config import settings
 from app.repositories import user_repo, spendings_repo
 from app.schemas.transactions_schemas import STransactionCreateInDB
 from app.schemas.user_schemas import SUserSignUp
 from app.services import user_spend_cat_service
+
+
+def sign_up_user(client: TestClient, username: str):
+    client.post(
+        url=f"{settings.api.prefix_v1}/sign_up/",
+        json={
+            "username": username,
+            "password": "password",
+            "email": f"{username.lower()}@example.com",
+        }
+    )
+
+
+def sign_in_user(client: TestClient, username: str):
+    client.post(
+        url=f"{settings.api.prefix_v1}/sign_in/",
+        data={
+            "username": username,
+            "password": "password",
+        }
+    )
 
 
 async def add_mock_user(db_session: AsyncSession, username: str) -> None:
