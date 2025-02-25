7c3edb13932af3503374f49d5c9321aaff80142c
csmqbusy <cosmiqeyes@yandex.by>
2025-02-25 18:42:26 +0300

    feat: add `BaseCategoriesRepository`

--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -1,11 +1,13 @@
 from .user_repository import user_repo
 from .base_transactions_repository import BaseTransactionsRepository
+from .base_categories_repository import BaseCategoriesRepository
 from .spendings_repository import spendings_repo
 from .users_spending_categories_repository import user_spend_cat_repo
 
 __all__ = [
     "user_repo",
     "BaseTransactionsRepository",
+    "BaseCategoriesRepository",
     "spendings_repo",
     "user_spend_cat_repo",
 ]
---
+++ app/repositories/base_categories_repository.py
@@ -0,0 +1,28 @@
+from typing import Type
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.base_catetgories_model import BaseCategoriesModel
+from app.repositories.base_repository import BaseRepository
+
+
+class BaseCategoriesRepository(BaseRepository[BaseCategoriesModel]):
+    def __init__(self, model: Type[BaseCategoriesModel]):
+        super().__init__(model=model)
+
+    async def get_category(
+        self,
+        session: AsyncSession,
+        user_id: int,
+        category_name: str,
+    ) -> BaseCategoriesModel | None:
+        query = (
+            select(self.model)
+            .filter(
+                self.model.category_name.ilike(category_name),
+                self.model.user_id == user_id,
+            )
+        )
+        result = await session.execute(query)
+        return result.scalar_one_or_none()
