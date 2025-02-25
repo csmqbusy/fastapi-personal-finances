f18ce683dff8c752781414b856c78e1b37255046
csmqbusy <cosmiqeyes@yandex.by>
2025-02-25 18:44:26 +0300

    refactor: change `user_spend_cat_repo` declaration
    - delete separate class `UserSpendingCategoriesRepository`
    - make `user_spend_cat_repo` an instance of `UserSpendingCategoriesRepository` class

--- app/repositories/users_spending_categories_repository.py
+++ app/repositories/users_spending_categories_repository.py
@@ -1,31 +1,6 @@
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
-
 from app.models import UsersSpendingCategoriesModel
-from app.repositories.base_repository import BaseRepository
+from app.repositories.base_categories_repository import BaseCategoriesRepository
 
-
-class UserSpendingCategoriesRepository(
-    BaseRepository[UsersSpendingCategoriesModel]
-):
-    def __init__(self):
-        super().__init__(UsersSpendingCategoriesModel)
-
-    async def get_category(
-        self,
-        session: AsyncSession,
-        user_id: int,
-        category_name: str,
-    ) -> UsersSpendingCategoriesModel | None:
-        query = (
-            select(self.model)
-            .filter(
-                self.model.category_name.ilike(category_name),
-                self.model.user_id == user_id,
-            )
-        )
-        user = await session.execute(query)
-        return user.scalar_one_or_none()
-
-
-user_spend_cat_repo = UserSpendingCategoriesRepository()
+user_spend_cat_repo = BaseCategoriesRepository(
+    model=UsersSpendingCategoriesModel,
+)
