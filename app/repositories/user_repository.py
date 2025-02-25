73dd5cfffb0a23c25e3090304f177303b7dc77ff
csmqbusy <cosmiqeyes@yandex.by>
2025-02-25 16:06:12 +0300

    refactor: add type annotations to `get_by_username` method

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,4 +1,5 @@
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import UserModel
 from app.repositories.base_repository import BaseRepository
@@ -8,7 +9,11 @@ class UserRepository(BaseRepository[UserModel]):
     def __init__(self):
         super().__init__(UserModel)
 
-    async def get_by_username(self, session, username):
+    async def get_by_username(
+        self,
+        session: AsyncSession,
+        username: str,
+    ) -> UserModel | None:
         query = select(self.model).filter(
             self.model.username.ilike(username)
         )
