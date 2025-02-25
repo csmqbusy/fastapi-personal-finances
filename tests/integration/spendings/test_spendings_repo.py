96d81dd8281f4eec8d4568b4443d208eedaa40e4
csmqbusy <cosmiqeyes@yandex.by>
2025-02-25 12:31:51 +0300

    test: add `test_get_transactions__with_all_filters` test

--- tests/integration/spendings/test_spendings_repo.py
+++ tests/integration/spendings/test_spendings_repo.py
@@ -356,3 +356,98 @@ async def test_get_transactions__with_amount_range(
         max_amount=max_amount,
     )
     assert len(spendings) == expected_spendings_qty
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    (
+        "category_name",
+        "create_user",
+        "amounts",
+        "descriptions",
+        "datetimes",
+        "min_amount",
+        "max_amount",
+        "search_term",
+        "datetime_from",
+        "datetime_to",
+        "sort_params",
+        "expected_spendings_qty",
+        "expected_result_entry_amount",
+    ),
+    [
+        (
+            "Food",
+            True,
+            [1000, 2000, 3000, 4000, 5000],
+            ["text", "TEXT ", "My Text", "text1", "t3xt"],
+            [
+                datetime(year=2020, month=1, day=1),
+                datetime(year=2021, month=1, day=1),
+                datetime(year=2022, month=1, day=1),
+                datetime(year=2023, month=1, day=1),
+                datetime(year=2024, month=1, day=1),
+            ],
+            2000,
+            10000,
+            "text",
+            datetime(year=2022, month=2, day=2),
+            datetime(year=2023, month=1, day=1),
+            [SortParam(order_by="amount", order_direction="desc")],
+            1,
+            4000,
+        ),
+    ]
+)
+async def test_get_transactions__with_all_filters(
+    db_session: AsyncSession,
+    category_name: str,
+    create_user: bool,
+    amounts: list[int],
+    descriptions: list[str],
+    datetimes: list[datetime],
+    min_amount: int,
+    max_amount: int,
+    search_term: str,
+    datetime_from: datetime,
+    datetime_to: datetime,
+    sort_params: list[SortParam],
+    expected_spendings_qty: int,
+    expected_result_entry_amount: int,
+) -> None:
+    mock_user_username = "TONALI"
+    if create_user:
+        await add_mock_user(db_session, mock_user_username)
+    user = await user_repo.get_by_username(db_session, mock_user_username)
+
+    category = await user_spend_cat_service.add_category_to_db(
+        user.id,
+        category_name,
+        db_session,
+    )
+
+    for i in range(len(amounts)):
+        transaction_to_create = STransactionCreateInDB(
+            amount=amounts[i],
+            description=descriptions[i],
+            date=datetimes[i],
+            user_id=user.id,
+            category_id=category.id,
+        )
+        await spendings_repo.add(
+            db_session,
+            transaction_to_create.model_dump(),
+        )
+
+    spendings = await spendings_repo.get_transactions(
+        query_params=dict(user_id=user.id, category_id=category.id),
+        min_amount=min_amount,
+        max_amount=max_amount,
+        description_search_term=search_term,
+        datetime_from=datetime_from,
+        datetime_to=datetime_to,
+        sort_params=sort_params,
+        session=db_session,
+    )
+    assert len(spendings) == expected_spendings_qty
+    assert spendings[0].amount == expected_result_entry_amount
