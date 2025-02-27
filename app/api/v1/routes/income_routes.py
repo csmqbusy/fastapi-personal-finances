99b812e3373e8565c909d665284f16a7166d92ea
csmqbusy <cosmiqeyes@yandex.by>
2025-02-27 17:19:49 +0300

    feat: add `GET /income/{income_id}/` route

--- app/api/v1/routes/income_routes.py
+++ app/api/v1/routes/income_routes.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.dependencies.auth_dependencies import get_active_verified_user
-from app.api.exceptions.operations_exceptions import CategoryNotFoundError
+from app.api.exceptions.operations_exceptions import CategoryNotFoundError, \
+    TransactionNotFoundError
 from app.db import get_db_session
 from app.exceptions.categories_exceptions import CategoryNotFound
+from app.exceptions.transaction_exceptions import TransactionNotFound
 from app.models import UserModel
 from app.schemas.transaction_category_schemas import STransactionCategoryOut
 from app.schemas.transactions_schemas import (
@@ -47,3 +49,23 @@ async def income_categories_get(
     db_session: AsyncSession = Depends(get_db_session),
 ) -> list[STransactionCategoryOut]:
     return await user_income_cat_service.get_user_categories(user.id, db_session)
+
+
+@router.get(
+    "/{income_id}/",
+    status_code=status.HTTP_200_OK,
+    summary="Get income details",
+)
+async def income_get(
+    income_id: int,
+    user: UserModel = Depends(get_active_verified_user),
+    db_session: AsyncSession = Depends(get_db_session),
+) -> STransactionResponse:
+    try:
+        return await income_service.get_transaction(
+            income_id,
+            user.id,
+            db_session,
+        )
+    except TransactionNotFound:
+        raise TransactionNotFoundError()
