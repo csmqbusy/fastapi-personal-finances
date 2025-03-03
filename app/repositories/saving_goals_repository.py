a2bf4d3aa6b3e236568d62a5a9125408b6637c87
csmqbusy <cosmiqeyes@yandex.by>
2025-03-03 18:10:28 +0300

    feat: add `get_goals_from_db` method to `SavingGoalsRepository`

--- app/repositories/saving_goals_repository.py
+++ app/repositories/saving_goals_repository.py
@@ -1,10 +1,92 @@
+from datetime import date
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.models import SavingGoalsModel
 from app.repositories.base_repository import BaseRepository
+from app.schemas.common_schemas import SortParam
+from app.schemas.saving_goals_schemas import GoalStatus
 
 
 class SavingGoalsRepository(BaseRepository[SavingGoalsModel]):
     def __init__(self):
         super().__init__(SavingGoalsModel)
+
+    async def get_goals_from_db(
+        self,
+        session: AsyncSession,
+        user_id: int,
+        min_current_amount: int | None = None,
+        max_current_amount: int | None = None,
+        min_target_amount: int | None = None,
+        max_target_amount: int | None = None,
+        name_search_term: str | None = None,
+        description_search_term: str | None = None,
+        start_date_from: date | None = None,
+        start_date_to: date | None = None,
+        target_date_from: date | None = None,
+        target_date_to: date | None = None,
+        end_date_from: date | None = None,
+        end_date_to: date | None = None,
+        status: GoalStatus | None = None,
+        sort_params: list[SortParam] | None = None,
+    ) -> list[SavingGoalsModel]:
+        query = (
+            select(self.model)
+            .where(self.model.user_id == user_id)
+        )
+
+        if name_search_term:
+            query = query.filter(
+                self.model.name.ilike(f"%{name_search_term}%")
+            )
+        if description_search_term:
+            query = query.filter(
+                self.model.description.ilike(f"%{description_search_term}%")
+            )
+
+        if min_current_amount:
+            query = query.where(self.model.current_amount >= min_current_amount)
+        if max_current_amount:
+            query = query.where(self.model.current_amount <= max_current_amount)
+
+        if min_target_amount:
+            query = query.where(self.model.target_amount >= min_target_amount)
+        if max_target_amount:
+            query = query.where(self.model.target_amount <= max_target_amount)
+
+        if start_date_from:
+            query = query.where(self.model.start_date >= start_date_from)
+        if start_date_to:
+            query = query.where(self.model.start_date <= start_date_to)
+
+        if target_date_from:
+            query = query.where(self.model.target_date >= target_date_from)
+        if target_date_to:
+            query = query.where(self.model.target_date <= target_date_to)
+
+        if end_date_from:
+            query = query.where(self.model.end_date >= end_date_from)
+        if end_date_to:
+            query = query.where(self.model.end_date <= end_date_to)
+
+        if status:
+            query = query.where(self.model.status == status)
+
+        if sort_params:
+            for param in sort_params:
+                if param.order_direction == "asc":
+                    query = query.order_by(
+                        getattr(self.model, param.order_by).asc()
+                    )
+                else:
+                    query = query.order_by(
+                        getattr(self.model, param.order_by).desc()
+                    )
+
+        result = await session.execute(query)
+        return list(result.scalars().all())
 
 
 saving_goals_repo = SavingGoalsRepository()
