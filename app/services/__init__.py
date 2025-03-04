96413222fd87e0c82207cc883e4f856f1678eefa
csmqbusy <cosmiqeyes@yandex.by>
2025-03-04 12:27:18 +0300

    refactor: update services `__init__.py`

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,8 +1,10 @@
 from .spendings_service import spendings_service
 from .users_spending_categories_service import user_spend_cat_service
+from .saving_goals_service import saving_goals_service
 
 
 __all__ = [
     "spendings_service",
     "user_spend_cat_service",
+    "saving_goals_service",
 ]
