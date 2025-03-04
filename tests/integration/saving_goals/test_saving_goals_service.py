b57db4882aa97a0a5d964686f54e49d0ae1a1dfe
csmqbusy <cosmiqeyes@yandex.by>
2025-03-04 13:30:19 +0300

    test: add `test_get_goal_progress` func to `test_saving_goals_service.py`

--- tests/integration/saving_goals/test_saving_goals_service.py
+++ tests/integration/saving_goals/test_saving_goals_service.py
@@ -458,4 +458,108 @@ async def test_update_goal(
             assert updated_goal.target_date == goal_update_obj.target_date
 
 
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    (
+        "create_user",
+        "name",
+        "description",
+        "current_amount",
+        "target_amount",
+        "start_date",
+        "expected_start_date",
+        "target_date",
+        "wrong_user_id",
+        "wrong_goal_id",
+        "expectation",
+    ),
+    [
+        (
+            True,
+            "Macbook Pro",
+            "Good laptop",
+            0,
+            200000,
+            date.fromisoformat("2025-01-01"),
+            date.fromisoformat("2025-01-01"),
+            date.fromisoformat("2025-06-20"),
+            None,
+            None,
+            nullcontext(),
+        ),
+        (
+            False,
+            "Macbook Pro",
+            "Good laptop",
+            0,
+            200000,
+            date.fromisoformat("2025-01-01"),
+            date.fromisoformat("2025-01-01"),
+            date.fromisoformat("2025-06-20"),
+            9999,
+            None,
+            pytest.raises(GoalNotFound),
+        ),
+        (
+            False,
+            "Macbook Pro",
+            "Good laptop",
+            0,
+            200000,
+            date.fromisoformat("2025-01-01"),
+            date.fromisoformat("2025-01-01"),
+            date.fromisoformat("2025-06-20"),
+            None,
+            9999,
+            pytest.raises(GoalNotFound),
+        ),
+    ]
+)
+async def test_get_goal_progress(
+    db_session: AsyncSession,
+    create_user: bool,
+    name: str,
+    description: str,
+    current_amount: int,
+    target_amount: int,
+    start_date: date,
+    expected_start_date: date,
+    target_date: date,
+    wrong_user_id: int | None,
+    wrong_goal_id: int | None,
+    expectation: ContextManager,
+):
+    mock_user_username = "50Messi"
+    if create_user:
+        await add_mock_user(db_session, mock_user_username)
+    user = await user_repo.get_by_username(db_session, mock_user_username)
+
+    goal = SSavingGoalCreate(
+        name=name,
+        description=description,
+        current_amount=current_amount,
+        target_amount=target_amount,
+        start_date=start_date,
+        target_date=target_date,
+    )
+    created_goal = await saving_goals_service.set_goal(
+        session=db_session,
+        goal=goal,
+        user_id=user.id,
+    )
+    assert created_goal is not None
+
+    with expectation:
+        goal_progress = await saving_goals_service.get_goal_progress(
+            goal_id=wrong_goal_id or created_goal.id,
+            user_id=wrong_user_id or user.id,
+            session=db_session,
+        )
+        assert goal_progress is not None
+        assert goal_progress.current_amount == goal.current_amount
+        assert goal_progress.target_amount == goal.target_amount
+        assert goal_progress.rest_amount == goal.target_amount - goal.current_amount
+        assert 0 <= goal_progress.percentage_progress <= 100
+        assert goal_progress.days_left is not None
+        assert goal_progress.expected_daily_payment is not None
 
