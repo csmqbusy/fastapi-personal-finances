a9576de5bf58b0d05d83bdd626060e921532379d
csmqbusy <cosmiqeyes@yandex.by>
2025-03-04 13:06:28 +0300

    test: add `test_delete_goal` func to `test_saving_goals_service.py`

--- tests/integration/saving_goals/test_saving_goals_service.py
+++ tests/integration/saving_goals/test_saving_goals_service.py
@@ -216,3 +216,114 @@ async def test_get_goal(
         assert goal_from_db.target_amount == goal.target_amount
         assert goal_from_db.start_date == expected_start_date
         assert goal_from_db.target_date == goal.target_date
+
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    (
+        "create_user",
+        "name",
+        "description",
+        "current_amount",
+        "target_amount",
+        "start_date",
+        "expected_start_date",
+        "target_date",
+        "wrong_goal_id",
+        "wrong_user_id",
+        "expectation",
+        "after_delete_expectation",
+    ),
+    [
+        (
+            True,
+            "Macbook Pro",
+            "Good laptop",
+            0,
+            200000,
+            date.fromisoformat("2025-01-01"),
+            date.fromisoformat("2025-01-01"),
+            date.fromisoformat("2025-06-20"),
+            None,
+            None,
+            nullcontext(),
+            pytest.raises(GoalNotFound),
+        ),
+        (
+            False,
+            "Macbook Pro",
+            "Good laptop",
+            0,
+            200000,
+            date.fromisoformat("2025-01-01"),
+            date.fromisoformat("2025-01-01"),
+            date.fromisoformat("2025-06-20"),
+            None,
+            9999,
+            pytest.raises(GoalNotFound),
+            pytest.raises(GoalNotFound),
+        ),
+        (
+            False,
+            "Macbook Pro",
+            "Good laptop",
+            0,
+            200000,
+            date.fromisoformat("2025-01-01"),
+            date.fromisoformat("2025-01-01"),
+            date.fromisoformat("2025-06-20"),
+            7777,
+            None,
+            pytest.raises(GoalNotFound),
+            pytest.raises(GoalNotFound),
+        ),
+    ]
+)
+async def test_delete_goal(
+    db_session: AsyncSession,
+    create_user: bool,
+    name: str,
+    description: str,
+    current_amount: int,
+    target_amount: int,
+    start_date: date,
+    expected_start_date: date,
+    target_date: date,
+    wrong_goal_id: int | None,
+    wrong_user_id: int | None,
+    expectation: ContextManager,
+    after_delete_expectation: ContextManager,
+):
+    mock_user_username = "30Messi"
+    if create_user:
+        await add_mock_user(db_session, mock_user_username)
+    user = await user_repo.get_by_username(db_session, mock_user_username)
+
+    goal = SSavingGoalCreate(
+        name=name,
+        description=description,
+        current_amount=current_amount,
+        target_amount=target_amount,
+        start_date=start_date,
+        target_date=target_date,
+    )
+    created_goal = await saving_goals_service.set_goal(
+        session=db_session,
+        goal=goal,
+        user_id=user.id,
+    )
+    assert created_goal is not None
+
+    with expectation:
+        await saving_goals_service.delete_goal(
+            goal_id=wrong_goal_id or created_goal.id,
+            user_id=wrong_user_id or user.id,
+            session=db_session,
+        )
+        with after_delete_expectation:
+            await saving_goals_service.get_goal(
+                goal_id=wrong_goal_id or created_goal.id,
+                user_id=wrong_user_id or user.id,
+                session=db_session,
+            )
