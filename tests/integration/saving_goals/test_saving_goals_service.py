8e59714e1c33de12acbe2ad3b1031b40e0ce623a
csmqbusy <cosmiqeyes@yandex.by>
2025-03-04 12:27:59 +0300

    test: add `test_set_goal` func to `test_saving_goals_service.py`

---
+++ tests/integration/saving_goals/test_saving_goals_service.py
@@ -0,0 +1,112 @@
+from contextlib import nullcontext
+from datetime import date
+from typing import ContextManager
+
+import pytest
+from pydantic import ValidationError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.repositories import user_repo
+from app.schemas.saving_goals_schemas import SSavingGoalCreate
+from app.services import saving_goals_service
+from tests.integration.helpers import add_mock_user
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    (
+        "create_user",
+        "name",
+        "description",
+        "current_amount",
+        "target_amount",
+        "start_date",
+        "expected_start_date",
+        "target_date",
+        "expectation",
+    ),
+    [
+        (
+            True,
+            "Macbook Pro",
+            "Good laptop",
+            0,
+            200000,
+            date.fromisoformat("2025-01-01"),
+            date.fromisoformat("2025-01-01"),
+            date.fromisoformat("2025-06-20"),
+            nullcontext(),
+        ),
+        (
+            False,
+            "Macbook Pro",
+            None,
+            0,
+            200000,
+            None,
+            date.today(),
+            date.fromisoformat("2025-06-20"),
+            nullcontext(),
+        ),
+        (
+            False,
+            "Macbook Pro",
+            None,
+            0,
+            200000,
+            date.fromisoformat("2026-01-01"),
+            date.fromisoformat("2026-01-01"),
+            date.fromisoformat("2025-06-20"),
+            pytest.raises(ValidationError),
+        ),
+        (
+            False,
+            None,
+            None,
+            0,
+            200000,
+            None,
+            date.today(),
+            date.fromisoformat("2025-06-20"),
+            pytest.raises(ValidationError),
+        ),
+    ]
+)
+async def test_set_goal(
+    db_session: AsyncSession,
+    create_user: bool,
+    name: str,
+    description: str,
+    current_amount: int,
+    target_amount: int,
+    start_date: date,
+    expected_start_date: date,
+    target_date: date,
+    expectation: ContextManager,
+):
+    mock_user_username = "10Messi"
+    if create_user:
+        await add_mock_user(db_session, mock_user_username)
+    user = await user_repo.get_by_username(db_session, mock_user_username)
+
+    with expectation:
+        goal = SSavingGoalCreate(
+            name=name,
+            description=description,
+            current_amount=current_amount,
+            target_amount=target_amount,
+            start_date=start_date,
+            target_date=target_date,
+        )
+        created_goal = await saving_goals_service.set_goal(
+            session=db_session,
+            goal=goal,
+            user_id=user.id,
+        )
+        assert created_goal is not None
+        assert created_goal.name == goal.name
+        assert created_goal.description == goal.description
+        assert created_goal.current_amount == goal.current_amount
+        assert created_goal.target_amount == goal.target_amount
+        assert created_goal.start_date == expected_start_date
+        assert created_goal.target_date == goal.target_date
