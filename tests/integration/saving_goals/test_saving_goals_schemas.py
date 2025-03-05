6bdf4611cd4085af4cd1842c05e247dadd1d3e7b
csmqbusy <cosmiqeyes@yandex.by>
2025-03-05 16:34:33 +0300

    test: add `test_SSavingGoalUpdatePartial_schema` func to `test_saving_goals_schemas.py`

--- tests/integration/saving_goals/test_saving_goals_schemas.py
+++ tests/integration/saving_goals/test_saving_goals_schemas.py
@@ -5,7 +5,10 @@ from typing import ContextManager
 import pytest
 from pydantic import ValidationError
 
-from app.schemas.saving_goals_schemas import SSavingGoalBase
+from app.schemas.saving_goals_schemas import (
+    SSavingGoalBase,
+    SSavingGoalUpdatePartial,
+)
 
 
 @pytest.mark.parametrize(
@@ -81,6 +84,90 @@ def test_SSavingGoalBase_schema(
             description=description,
             current_amount=0,
             target_amount=100,
+            start_date=start_date,
+            target_date=target_date,
+        )
+        assert goal.name == expected_name
+        assert goal.description == expected_descriptions
+
+
+
+@pytest.mark.parametrize(
+    (
+        "name",
+        "description",
+        "current_amount",
+        "target_amount",
+        "start_date",
+        "target_date",
+        "expected_name",
+        "expected_descriptions",
+        "expectation",
+    ),
+    [
+        (
+            "name",
+            "description",
+            0,
+            1000,
+            date(2025, 1, 1),
+            date(2025, 12, 1),
+            "name",
+            "description",
+            nullcontext(),
+        ),
+        (
+            "name",
+            "description",
+            0,
+            1000,
+            date(2025, 12, 1),
+            date(2025, 1, 1),
+            "name",
+            "description",
+            pytest.raises(ValidationError),
+        ),
+        (
+            "name",
+            "description",
+            10000,
+            1000,
+            date(2025, 1, 1),
+            date(2025, 12, 1),
+            "name",
+            "description",
+            pytest.raises(ValidationError),
+        ),
+        (
+            "   name",
+            "description   ",
+            1000,
+            1000,
+            date(2025, 1, 1),
+            date(2025, 12, 1),
+            "name",
+            "description",
+            nullcontext(),
+        ),
+    ]
+)
+def test_SSavingGoalUpdatePartial_schema(
+    name: str,
+    description: str,
+    current_amount: int,
+    target_amount: int,
+    start_date: date,
+    target_date: date,
+    expected_name: str,
+    expected_descriptions: str,
+    expectation: ContextManager,
+):
+    with expectation:
+        goal = SSavingGoalUpdatePartial(
+            name=name,
+            description=description,
+            current_amount=current_amount,
+            target_amount=target_amount,
             start_date=start_date,
             target_date=target_date,
         )
