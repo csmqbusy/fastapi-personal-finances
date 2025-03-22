87dd22d16ada36db6c711ba1f6ca026a7dafc1c4
csmqbusy <cosmiqeyes@yandex.by>
2025-03-22 13:25:31 +0300

    refactor: code formatting in db

--- app/db/dependencies.py
+++ app/db/dependencies.py
@@ -6,11 +6,11 @@ from app.core.config import settings
 from app.db.manager import DatabaseSessionManager
 
 database_manager = DatabaseSessionManager(
-        url=str(settings.db.url),
-        echo=settings.db.echo,
-        echo_pool=settings.db.echo_pool,
-        pool_size=settings.db.pool_size,
-        max_overflow=settings.db.max_overflow,
+    url=str(settings.db.url),
+    echo=settings.db.echo,
+    echo_pool=settings.db.echo_pool,
+    pool_size=settings.db.pool_size,
+    max_overflow=settings.db.max_overflow,
 )
 
 
--- app/db/manager.py
+++ app/db/manager.py
@@ -10,27 +10,28 @@ from sqlalchemy.ext.asyncio import (
 
 class DatabaseSessionManager:
     def __init__(
-            self,
-            url: str,
-            echo: bool = False,
-            echo_pool: bool = False,
-            pool_size: int = 5,
-            max_overflow: int = 10
+        self,
+        url: str,
+        echo: bool = False,
+        echo_pool: bool = False,
+        pool_size: int = 5,
+        max_overflow: int = 10,
     ):
         self.engine: AsyncEngine = create_async_engine(
-                url=url,
-                echo=echo,
-                echo_pool=echo_pool,
-                pool_size=pool_size,
-                max_overflow=max_overflow
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+            pool_size=pool_size,
+            max_overflow=max_overflow,
         )
 
-        self.session_factory: async_sessionmaker[
-            AsyncSession] = async_sessionmaker(
+        self.session_factory: async_sessionmaker[AsyncSession] = (
+            async_sessionmaker(
                 bind=self.engine,
                 autoflush=False,
                 autocommit=False,
-                expire_on_commit=False
+                expire_on_commit=False,
+            )
         )
 
     async def dispose(self) -> None:
